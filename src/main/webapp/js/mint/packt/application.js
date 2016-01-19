b2d4406d8506e987b0cb344ab3b1ef3893a8c821
Cachot <a332831174@icloud.com>
2016-01-19 14:36:18 +0800

    packt

---
+++ src/main/webapp/js/mint/packt/application.js
@@ -0,0 +1,27 @@
+Ext.define('packt.Application',{  //1  means application has single page
+	extend: 'Ext.app.Application',
+	name: 'packt',  //2  
+	views: [  //3
+	
+	],
+	
+	controllers: [  //4
+	    'Root'
+	],
+	
+	stores: [  //5
+	
+	],
+	
+	launch: function(){  //6  this will be called after all the application's controllers are initialized
+		//TODO - Launch the application
+	},
+	
+	init: function() {
+		var me = this; // 1 reference to Ext.appliccation itself
+		me.splashscreen = Ext.getBody().mask( // 2
+			'Loading application', 'splashscreen'
+			);
+		}
+	}
+});
